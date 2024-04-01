94ca8eac75bca4eda61ab1d959399a7649e57761
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-04-01 18:24:43 +0700

    feat: add socketio

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,16 @@ dotenv.config({ path: path.join(__dirname, "../.env") });
 
 import express, { Express } from "express";
 import cors from "cors";
+import http from "http";
 import { login, register } from "./controllers/auth";
 import { Return } from "./utils/async";
 import { MongoDB } from "./database/mongo";
+import { Server, Socket } from "socket.io";
 
 async function main() {
   const app: Express = express();
+  const httpServer = http.createServer(app);
+  const io = new Server(httpServer);
 
   const PORT = process.env.PORT || 3000;
   const MONGO_URI = process.env.MONGO_URI || "";
@@ -21,8 +25,13 @@ async function main() {
     process.exit();
   }
 
+  io.on("connection", (socket: Socket) => {
+    socket.emit("hello", "world");
+  });
+
   app.use(cors());
   app.use(express.json());
+  app.use("/", express.static(path.join(__dirname, "../public/")));
 
   const api = express.Router();
   api.post("/auth/login", login);
@@ -30,7 +39,7 @@ async function main() {
 
   app.use("/api", api);
 
-  app.listen(PORT, () => {
+  httpServer.listen(PORT, () => {
     console.log(`[server] Server is running at http://localhost:${PORT}`);
   });
 }
