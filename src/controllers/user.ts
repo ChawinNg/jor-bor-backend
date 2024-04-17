4f1876f10e31ca7dfe340cd47d464e6f340a904f
ChawinNg <ik.chawin@gmail.com>
2024-04-17 15:57:48 +0700

    feat: get current user

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import { MongoDB } from "../database/mongo";
 import { IUser } from "../models/user";
-import { PromiseGuard } from "../utils/error";
+import { Guard, PromiseGuard } from "../utils/error";
+import { ObjectId } from "mongodb";
 
 export async function getAllUsers(req: Request, res: Response) {
   let usersQuery = MongoDB.db().collection<IUser>("users").find().toArray();
@@ -19,3 +20,18 @@ export async function getAllUsers(req: Request, res: Response) {
     }))
   );
 }
+
+export async function getMe(req: Request, res: Response) {
+  let { error: idErr, value: _id } = Guard(() => ObjectId.createFromHexString(res.locals.userId));
+  if (idErr !== undefined)
+    return res.status(401).send({ message: "required login" });
+
+  let query = MongoDB.db().collection<IUser>("users").findOne({ _id })
+  let { error: userErr, value: user } = await PromiseGuard(query)
+  if (userErr !== undefined)
+    return res.status(500).send({ message: userErr.message });
+  else if (user === null)
+    return res.status(404).send({ message: "user not found" });
+
+  return res.status(200).send({ data: { _id: user._id, username: user.username } })
+};
